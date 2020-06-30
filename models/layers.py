fb5ab642d51405c4c49f0849eed295a8c41bbe74
ZJUGiveLab <41609342+ZJUGiveLab@users.noreply.github.com>
2020-06-30 18:51:30 +0800

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -39,6 +39,29 @@ class unetConv2(nn.Module):
             x = conv(x)
 
         return x
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size, is_deconv, n_concat=2):
+        super(unetUp, self).__init__()
+        # self.conv = unetConv2(in_size + (n_concat - 2) * out_size, out_size, False)
+        self.conv = unetConv2(out_size*2, out_size, False)
+        if is_deconv:
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=4, stride=2, padding=1)
+        else:
+            self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+        # initialise the blocks
+        for m in self.children():
+            if m.__class__.__name__.find('unetConv2') != -1: continue
+            init_weights(m, init_type='kaiming')
+
+    def forward(self, inputs0, *input):
+        # print(self.n_concat)
+        # print(input)
+        outputs0 = self.up(inputs0)
+        for i in range(len(input)):
+            outputs0 = torch.cat([outputs0, input[i]], 1)
+        return self.conv(outputs0)
     
 class unetUp_origin(nn.Module):
     def __init__(self, in_size, out_size, is_deconv, n_concat=2):
